023dfea916198436292fb9829407702312330a1e
Katarzyna Mielnik <kasia.a.mielnik@gmail.com>
2022-11-23 18:39:27 +0100

    added missing implementation

--- src/concurrentcube/Cube.java
+++ src/concurrentcube/Cube.java
@@ -1,11 +1,26 @@
 package concurrentcube;
 
+import java.util.concurrent.Semaphore;
 import java.util.function.BiConsumer;
 
 public class Cube {
 
+    private final BiConsumer<Integer, Integer> beforeRotation;
+    private final BiConsumer<Integer, Integer> afterRotation;
+    private final Runnable beforeShowing;
+    private final Runnable afterShowing;
+
+
     private final int size;
     private final Side[] sides;
+    private static final int SHOW_PLANE_TYPE = 3;
+
+    private final Semaphore cubeAccess; //< mutex
+    private final int[] workingProcesses; //< number of processes working on each plane
+    private final int[] waitingProcesses; //< number of processes waiting to access each plane
+
+    private final Semaphore[] planeAccess;
+    private final Semaphore[] layerAccess;
 
     public Cube(int size,
                 BiConsumer<Integer, Integer> beforeRotation,
@@ -13,78 +28,171 @@ public class Cube {
                 Runnable beforeShowing,
                 Runnable afterShowing
     ) {
+
+        this.beforeRotation = beforeRotation;
+        this.afterRotation = afterRotation;
+        this.beforeShowing = beforeShowing;
+        this.afterShowing = afterShowing;
         this.size = size;
         this.sides = new Side[6];
         for (int i = 0; i <= 5; i++) {
             sides[i] = new Side(size, i);
         }
+
+        cubeAccess = new Semaphore(1, true);
+        waitingProcesses = new int[4];
+        workingProcesses = new int[4];
+        planeAccess = new Semaphore[4];
+        for (int i = 0; i < 4; i++) {
+            planeAccess[i] = new Semaphore(0, true);
+            workingProcesses[i] = 0;
+            waitingProcesses[i] = 0;
+        }
+        layerAccess = new Semaphore[size];
+        for (int i = 0; i < size; i++) {
+            layerAccess[i] = new Semaphore(1, true);
+        }
     }
 
-    public void rotate(int side, int layer) throws InterruptedException {
-
+    private void rotateCube(int side, int layer) {
         switch (side) {
             case 0:
-                sides[1].rotateRow(layer, sides[2].getSquares());
-                sides[2].rotateRow(layer, sides[3].getSquares());
-                sides[3].rotateRow(layer, sides[4].getSquares());
+                sides[1].swapRows(layer, sides[2]);
+                sides[2].swapRows(layer, sides[3]);
+                sides[3].swapRows(layer, sides[4]);
                 break;
             case 5:
-                sides[2].rotateRow(size - 1 - layer, sides[1].getSquares());
-                sides[1].rotateRow(size - 1 - layer, sides[4].getSquares());
-                sides[4].rotateRow(size - 1 - layer, sides[3].getSquares());
+                sides[2].swapRows(size - 1 - layer, sides[1]);
+                sides[1].swapRows(size - 1 - layer, sides[4]);
+                sides[4].swapRows(size - 1 - layer, sides[3]);
                 break;
             case 2:
-                sides[0].swapRowColumn(size - 1 - layer, size - 1 - layer, sides[1].getSquares(), true);
-                sides[1].swapColumnRow(size - 1 - layer, layer, sides[5].getSquares(), false);
-                sides[5].swapRowColumn(layer, layer, sides[3].getSquares(), true);
+                sides[0].swapRowColumn(size - 1 - layer, size - 1 - layer, sides[1], true);
+                sides[1].swapColumnRow(size - 1 - layer, layer, sides[5], false);
+                sides[5].swapRowColumn(layer, layer, sides[3], true);
                 break;
             case 4:
-                sides[0].swapRowColumn(layer, size - 1 - layer, sides[3].getSquares(), false);
-                sides[3].swapColumnRow(size - 1 - layer, size - 1 - layer, sides[5].getSquares(), true);
-                sides[5].swapRowColumn(size - 1 - layer, layer, sides[1].getSquares(), false);
+                sides[0].swapRowColumn(layer, size - 1 - layer, sides[3], false);
+                sides[3].swapColumnRow(size - 1 - layer, size - 1 - layer, sides[5], true);
+                sides[5].swapRowColumn(size - 1 - layer, layer, sides[1], false);
                 break;
             case 3:
-                sides[0].rotateColumn(size - 1 - layer, sides[2].getSquares(), false, false);
-                sides[2].rotateColumn(size - 1 - layer, sides[5].getSquares(), false, false);
-                sides[5].rotateColumn(size - 1 - layer, sides[4].getSquares(), true, true);
+                sides[0].swapColumns(size - 1 - layer, sides[2], false, false);
+                sides[2].swapColumns(size - 1 - layer, sides[5], false, false);
+                sides[5].swapColumns(size - 1 - layer, sides[4], true, true);
                 break;
             case 1:
-                sides[0].rotateColumn(layer, sides[4].getSquares(), true, true);
-                sides[4].rotateColumn(size - 1 - layer, sides[5].getSquares(), true, true);
-                sides[5].rotateColumn(layer, sides[2].getSquares(), false, false);
+                sides[0].swapColumns(layer, sides[4], true, true);
+                sides[4].swapColumns(size - 1 - layer, sides[5], true, true);
+                sides[5].swapColumns(layer, sides[2], false, false);
                 break;
-            default:
-                throw new InterruptedException();
         }
 
         if (layer == 0)
             sides[side].rotateClockwise();
         else if (layer == size - 1) {
-            sides[SideNumber.getOppositeSide(side)].rotateCounterclockwise();
+            sides[Side.getOppositeSide(side)].rotateCounterclockwise();
         }
-        else if (layer >= size || layer < 0){
-            // wywala exception
+    }
+
+    private boolean anyOtherPlanesWaiting(int plane) {
+        return waitingProcesses[(plane + 1) % 4] > 0
+                || waitingProcesses[(plane + 2) % 4] > 0
+                || waitingProcesses[(plane + 3) % 4] > 0;
+    }
+
+    private boolean anyOtherPlanesWorking(int plane) {
+        return workingProcesses[(plane + 1) % 4] > 0
+                || workingProcesses[(plane + 2) % 4] > 0
+                || workingProcesses[(plane + 3) % 4] > 0;
+    }
+
+    private void entryProcedure(int plane) throws InterruptedException {
+        cubeAccess.acquire();
+        if (anyOtherPlanesWaiting(plane) || anyOtherPlanesWorking(plane)) {
+            waitingProcesses[plane]++;
+            cubeAccess.release();
+            planeAccess[plane].acquireUninterruptibly();
+            waitingProcesses[plane]--;
         }
+
+        workingProcesses[plane]++;
+
+        if (waitingProcesses[plane] > 0)
+            planeAccess[plane].release();
+        else
+            cubeAccess.release();
+    }
+
+    private void afterFoo(int plane) throws InterruptedException {
+        cubeAccess.acquireUninterruptibly();
+        workingProcesses[plane]--;
+
+        if (workingProcesses[plane] == 0) {
+            if (waitingProcesses[(plane + 1) % 4] > 0)
+                planeAccess[(plane + 1) % 4].release();
+            else if (waitingProcesses[(plane + 2) % 4] > 0)
+                planeAccess[(plane + 2) % 4].release();
+            else if (waitingProcesses[(plane + 3) % 4] > 0)
+                planeAccess[(plane + 3) % 4].release();
+            else if (waitingProcesses[(plane + 4) % 4] > 0)
+                planeAccess[(plane + 4) % 4].release();
+            else
+                cubeAccess.release();
+        }
+        else
+            cubeAccess.release();
+
+        if (Thread.currentThread().isInterrupted())
+            throw new InterruptedException();
+    }
+
+
+    public void rotate(int side, int layer) throws InterruptedException {
+        int plane = sides[side].getPlaneNumber();
+        int planeLayer = Side.getLayer(side, layer, size);
+
+        entryProcedure(plane);
+
+        try {
+            if (!Thread.currentThread().isInterrupted()) {
+                layerAccess[planeLayer].acquire();
+
+                beforeRotation.accept(side, layer);
+                rotateCube(side, layer);
+                afterRotation.accept(side, layer);
+
+                layerAccess[planeLayer].release();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            afterFoo(plane);
+        }
+        if (Thread.currentThread().isInterrupted())
+            throw new InterruptedException();
     }
 
     public String show() throws InterruptedException {
+        entryProcedure(SHOW_PLANE_TYPE);
+
+        String s = "";
         StringBuilder b = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            sides[i].show(b);
+        if (!Thread.currentThread().isInterrupted()) {
+            beforeShowing.run();
+            for (int i = 0; i < 6; i++) {
+                sides[i].show(b);
+            }
+            s = b.toString();
+            afterShowing.run();
         }
 
-        String s = b.toString();
 
-//        for (int i = 0; i < 6; i++) {
-//            for (int j = 0; j < size; j++) {
-//                for (int k = 0; k < size; k++)
-//                    System.out.print(s.charAt(i * size * size + j * size + k));
-//                System.out.println(" ");
-//            }
-//
-//        }
-        return s;
+        afterFoo(SHOW_PLANE_TYPE);
+        if (Thread.currentThread().isInterrupted()) {
+            throw new InterruptedException();
+        }
+        else
+            return s;
     }
-
-
 }
--- src/concurrentcube/Side.java
+++ src/concurrentcube/Side.java
@@ -3,18 +3,52 @@ package concurrentcube;
 public class Side {
 
     private final int size;
-    private final SideNumber sideNumber;
+    private final int sideNumber;
     private final int[][] squares;
 
     public Side(int size, int sideNumber) {
         this.size = size;
-        this.sideNumber = SideNumber.getSideType(sideNumber);
+        this.sideNumber = sideNumber;
 
         squares = new int[size][];
         for (int i = 0; i < size; i++) {
             squares[i] = new int[size];
             for (int j = 0; j < size; j++)
                 squares[i][j] = sideNumber;
+        }
+    }
+
+    public static int getOppositeSide(int a) {
+        switch (a) {
+            case 0:
+                return 5;
+            case 5:
+                return 0;
+            case 2:
+                return 4;
+            case 4:
+                return 2;
+            case 3:
+                return 1;
+            case 1:
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    public static int getLayer(int side, int layer, int sideSize) {
+        switch (side) {
+            case 0:
+            case 1:
+            case 4:
+                return layer;
+            case 2:
+            case 3:
+            case 5:
+                return sideSize - layer - 1;
+            default:
+                return -1;
         }
     }
 
@@ -29,39 +63,31 @@ public class Side {
     }
 
     public void rotateClockwise() {
-
-        for (int i = 0; i <= size / 2; i++) {
-           int endIdx = size - i - 1;
-
-            for (int j = i; j < endIdx; j++) {
-                int acc = squares[j][i];
-                squares[j][i] = squares[endIdx][j];
-
-                squares[endIdx][j] = squares[endIdx - j][endIdx];
-                squares[endIdx - j][endIdx] = squares[i][endIdx - j];
-                squares[i][endIdx - j] = acc;
+        for (int i = 0; i < size / 2; i++) {
+            for (int j = i; j < size - i - 1; j++) {
+                int acc = squares[i][j];
+                squares[i][j] = squares[size - 1 - j][i];
+                squares[size - 1 - j][i] = squares[size - 1 - i][size - 1 - j];
+                squares[size - 1 - i][size - 1 - j] = squares[j][size - 1 - i];
+                squares[j][size - 1 - i] = acc;
             }
-
         }
     }
+
 
     public void rotateCounterclockwise() {
-        for (int i = 0; i <= size / 2; i++) {
-            int endIdx = size - i - 1;
-
-            for (int j = i; j < endIdx ; j++) {
+        for (int i = 0; i < size / 2; i++) {
+            for (int j = i; j < size - i - 1; j++) {
                 int acc = squares[i][j];
-
-                squares[i][j] = squares[j][endIdx];
-                squares[j][endIdx] = squares[endIdx][endIdx - j];
-                squares[endIdx][endIdx - j] = squares[endIdx - j][i];
-                squares[endIdx - j][i] = acc;
+                squares[i][j] = squares[j][size - 1 - i];
+                squares[j][size - 1 - i] = squares[size - 1 - i][size - 1 - j];
+                squares[size - 1 - i][size - 1 - j] = squares[size - 1 - j][i];
+                squares[size - 1 - j][i] = acc;
             }
-
         }
     }
 
-    public void rotateColumn(int columnIndex, int[][] swapSide, boolean reverseIndex, boolean oppositeColumn) {
+    public void swapColumns(int columnIndex, Side otherSide, boolean reverseIndex, boolean oppositeColumn) {
         int acc;
         for (int i = 0; i < size; i++) {
             int index = i;
@@ -73,34 +99,34 @@ public class Side {
                 swappedColumnIndex = size - 1 - columnIndex;
 
             acc = squares[i][columnIndex];
-            squares[i][columnIndex] = swapSide[index][swappedColumnIndex];
-            swapSide[index][swappedColumnIndex] = acc;
+            squares[i][columnIndex] = otherSide.squares[index][swappedColumnIndex];
+            otherSide.squares[index][swappedColumnIndex] = acc;
         }
     }
 
-    public void rotateRow(int rowNumber, int[][] swapSide) {
+    public void swapRows(int rowNumber, Side otherSide) {
         int acc;
         for (int i = 0; i < size; i++) {
             acc = squares[rowNumber][i];
-            squares[rowNumber][i] = swapSide[rowNumber][i];
-            swapSide[rowNumber][i] = acc;
+            squares[rowNumber][i] = otherSide.squares[rowNumber][i];
+            otherSide.squares[rowNumber][i] = acc;
         }
     }
 
-    public void swapColumnRow(int columnIndex, int rowIndex, int[][] swapSide, boolean reverseIndex) {
+    public void swapColumnRow(int columnIndex, int rowIndex, Side otherSide, boolean reverseIndex) {
         int acc;
         for (int i = 0; i < size; i++) {
             int index = i;
             if (reverseIndex)
-                index = size - 1 - i;
+                index = size - 1 - index;
 
             acc = squares[i][columnIndex];
-            squares[i][columnIndex] = swapSide[rowIndex][index];
-            swapSide[rowIndex][index] = acc;
+            squares[i][columnIndex] = otherSide.squares[rowIndex][index];
+            otherSide.squares[rowIndex][index] = acc;
         }
     }
 
-    public void swapRowColumn(int rowIndex, int columnIndex, int[][] swapSide, boolean reverseIndex) {
+    public void swapRowColumn(int rowIndex, int columnIndex, Side otherSide, boolean reverseIndex) {
         int acc;
         for (int i = 0; i < size; i++) {
             int index = i;
@@ -108,12 +134,24 @@ public class Side {
                 index = size - 1 - index;
 
             acc = squares[rowIndex][i];
-            squares[rowIndex][i] = swapSide[index][columnIndex];
-            swapSide[index][columnIndex] = acc;
+            squares[rowIndex][i] = otherSide.squares[index][columnIndex];
+            otherSide.squares[index][columnIndex] = acc;
         }
     }
 
-    int[][] getSquares() {
-        return squares;
+    int getPlaneNumber() {
+        switch (sideNumber) {
+            case 0:
+            case 5:
+                return 0;
+            case 1:
+            case 3:
+                return 1;
+            case 2:
+            case 4:
+                return 2;
+        }
+        return -1;
     }
+
 }
